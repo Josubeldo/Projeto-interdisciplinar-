b70fc2a1ba97d06862b4ff151e9e810a0c5559e6
Matheus Vieira <vieiramatheus@yahoo.com.br>
2025-05-27 20:45:42 -0300

    adicionando paginas pré-criadas

--- index.php
+++ index.php
@@ -1,3 +1,172 @@
-<?php 
-echo "OLA MUNDÃO DE MEU DEUS"
-?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Nutri Kids</title>
+</head>
+<body>
+    <!--CABEÇALHO-->
+    <header class="cabecalho">
+        <h2 class="titulo">Nutri Kids</h2>
+        <nav class="menu">
+            <a href="pagina-1.html">Médicos</a>
+            <a href="pagina-2.html">Pacientes</a>
+            <a href="pagina-3.html">Login</a>
+            <a href="pagina-4.html">Informações</a>
+            <a href="index.html" class="voltar_menu">home</a>
+        </nav>    
+    </header>
+    <!--FIM CABEÇALHO-->
+    
+    <!--CORPO DA PÁGINA-->
+    <!--SESSÃO 1-->
+    <main class="conteudo">
+        <h1 class="titulo-principal">Landing page title</h1>
+        <p class="p1">Subheading that sets up context, shares more info about the website, or generally gets people 
+           psyched to keep scrolling.</p>
+           <nav class="botao">
+                <a href="#" class="botao1">Button</a>
+           </nav>
+        <img src="imagens/roxo.jpg" alt="Kiwi" class="kiwi" >
+    <!-- FIM SESSÃO 1-->
+        
+    <!--SESSÃO 2-->
+        <h2 class="titulo2">Section heading</h2>
+        <section class="tres-img-agrupadas">
+            <div class="item-tres-img">
+                <img src="imagens/roxo.jpg">
+                <p>Subheading</p>
+                <p class="paragrafo-cinza">Body text for whatever you’d like to add more to the subheading.</p>
+            </div>
+            <div class="item-tres-img">
+                <img src="imagens/roxo.jpg">
+                <p>Subheading</p>
+                <p class="paragrafo-cinza">Body text for whatever you’d like to add more to the subheading.</p>
+            </div>
+            <div class="item-tres-img">
+                <img src="imagens/roxo.jpg">
+                <p>Subheading</p>
+                <p class="paragrafo-cinza">Body text for whatever you’d like to add more to the subheading.</p>
+            </div>
+        </section>
+    <!-- FIM SESSÃO 2-->
+
+    <!--SESSÃO 3-->
+        <section class="texto-imagem">
+            <div class="texto-esquerda">
+                <h2 class="texto3">Section heading</h2>
+                <p>Subheading</p>
+                <p class="paragrafo-cinza">Body text for whatever you’d like to expand on the main point. </p>
+                <p>Subheading</p>
+                <p class="paragrafo-cinza">Body text for whatever you’d like to expand on the main point. </p>
+                <p>Subheading</p>
+                <p class="paragrafo-cinza">Body text for whatever you’d like to expand on the main point. </p>
+                <div class="botoes">
+                    <a href="#" class="botao2">Button</a>
+                    <a href="#" class="botao3">Secondary Button</a>
+                </div>
+            </div>
+            <div class="imagem-direita">
+                <img src="imagens/roxo.jpg" alt="Roxo">
+            </div>
+        </section>
+    <!--FIM SESSÃO 3-->
+
+    <!--SESSÃO 4-->
+        <h2 class="titulo4">Section heading</h2>
+        <section class="duas-img-agrupadas">
+            <div class="item-duas-img">
+                <img src="imagens/roxo.jpg">
+                <p>Subheading</p>
+                <p class="paragrafo-cinza">Body text for whatever you’d like to add more to the subheading.</p>
+            </div>
+            <div class="item-duas-img">
+                <img src="imagens/roxo.jpg">
+                <p>Subheading</p>
+                <p class="paragrafo-cinza">Body text for whatever you’d like to add more to the subheading.</p>
+            </div>
+        </section>
+    <!--FIM SESSÃO 4-->
+        
+    <!--SESSÃO 5-->
+        <h2 class="texto5">Section heading</h2>
+        <section class="desenvolvedores">
+            <div class="dev-box">
+                <h3 class="descricao">A terrific piece of praise</h3>
+                <div class="dev-info">
+                    <img src="imagens/roxo.jpg" alt="roxo">
+                    <p>Nome<br><small class="paragrafo-cinza">Função / Cargo</small></p>
+                </div>
+            </div>
+            <div class="dev-box">
+                <h3 class="descricao">A fantastic bit of feedback</h3>
+                <div class="dev-info">
+                    <img src="imagens/roxo.jpg" alt="roxo">
+                    <p>Nome<br><small class="paragrafo-cinza">Função / Cargo</small></p>
+                </div>
+            </div>
+            <div class="dev-box">
+                <h3 class="descricao">A genuinely glowing review</h3>
+                <div class="dev-info">
+                    <img src="imagens/roxo.jpg" alt="roxo">
+                    <p>Nome<br><small class="paragrafo-cinza">Função / Cargo</small></p>
+                </div>
+            </div>
+        </section>
+    <!--FIM SESSÃO 5-->
+
+    <!--SESSÃO 6-->
+        <section class="faixa-cinza">
+            <div class="texto-esquerdo">
+                <h2>Section heading</h2>
+            </div>
+            <div class="botoes-direitos">
+                <a href="#" class="botao2">Button</a>
+                <a href="#" class="botao3">Secondary Button</a>
+            </div>
+        </section>
+    <!--FIM SESSÃO 6-->
+    </main>
+    <!--FIM CORPO DA PÁGINA-->
+
+    <!--RODAPÉ ESQUERDO-->
+    <footer class="rodape">
+        <div class="rodape-esquerdo">
+            <h3>Site name</h3>
+            <div class="icones">
+                <img src="imagens/roxo.jpg" alt="roxo">
+                <img src="imagens/roxo.jpg" alt="roxo">
+                <img src="imagens/roxo.jpg" alt="roxo">
+                <img src="imagens/roxo.jpg" alt="roxo">
+            </div>
+        </div>
+    <!--RODAPÉ DIREITO-->
+        <div class="rodape-direito">
+            <div class="linha">
+                <p>Topic</p>
+                <p>Topic</p>
+                <p>Topic</p>
+            </div>
+            <div class="linha">
+                <p class="paragrafo-cinza">Page</p>
+                <p class="paragrafo-cinza">Page</p>
+                <p class="paragrafo-cinza">Page</p>
+            </div>
+            <div class="linha">
+                <p class="paragrafo-cinza">Page</p>
+                <p class="paragrafo-cinza">Page</p>
+                <p class="paragrafo-cinza">Page</p>
+            </div>
+            <div class="linha">
+                <p class="paragrafo-cinza">Page</p>
+                <p class="paragrafo-cinza">Page</p>
+                <p class="paragrafo-cinza">Page</p>
+            </div>
+        </div>
+    </footer>
+    <!--FIM RODAPÉ-->
+
+</body>
+</html>
